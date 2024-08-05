50a48e6c247574b7c2ccb010a28d22112aa51361
bmohideen <70986859+bmohideen@users.noreply.github.com>
2024-08-05 16:47:38 -0400

    Fixed grammatical error in title

--- main.R
+++ main.R
@@ -1,4 +1,4 @@
-# Midterm = Bay Area Bike Rental Operation Research Project
+# Midterm - Bay Area Bike Rental Operation Research Project
 # Bilal Mohideen - BTC1855H
 # RStudio Version 2024.04.1+748 (2024.04.1+748)
 
