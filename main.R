1f83b56ff8a8dc37253eedded87931b9330f5b5c
bmohideen <70986859+bmohideen@users.noreply.github.com>
2024-08-05 18:09:05 -0400

    Loading required packages and importing the datasets

--- main.R
+++ main.R
@@ -2,3 +2,20 @@
 # Bilal Mohideen - BTC1855H
 # RStudio Version 2024.04.1+748 (2024.04.1+748)
 
+# loading the required packages for the project
+library(funModeling)
+library(Hmisc)
+library(tidyverse)
+library(lubridate)
+library(corrplot)
+
+# importing the datasets in as data frames
+station <- read.csv("station.csv")
+trip <- read.csv("trip.csv")
+weather <- read.csv("weather.csv")
+
+#### EDA ####
+
+# performing exploratory data analysis (EDA) for each dataset separately
+# following template outlined here: 
+#https://blog.datascienceheroes.com/exploratory-data-analysis-in-r-intro/
