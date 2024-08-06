a9e8ee4edb28f5c9f169dbf909b0a6600b95fe21
bmohideen <70986859+bmohideen@users.noreply.github.com>
2024-08-06 13:19:31 -0400

    Data cleaning for the trip dataset

--- main.R
+++ main.R
@@ -135,3 +135,57 @@ station_v1$installation_date <- mdy(station_v1$installation_date,
 # check for the count of duplicate rows
 # all rows are distinct (returns FALSE)
 any(duplicated(station_v1))
+
+#### Data Cleaning - Trip ####
+# replace blank values with NA in the zip_code column of dataset
+# zip_code will not be used for downstream analysis, so rows with NA 
+# do not need to be removed
+trip_v1 <- trip_v1 %>%
+  mutate(zip_code = na_if(zip_code, ""))
+
+# check for the count of duplicate rows
+# all rows are distinct (returns FALSE)
+any(duplicated(trip_v1))
+
+# convert start_date and end_date to POSIX format from character type
+trip_v1$start_date <- mdy_hm(trip_v1$start_date, 
+                             tz = "UTC")
+trip_v1$end_date <- mdy_hm(trip_v1$end_date, 
+                             tz = "UTC")
+
+# recording trip IDs for cancelled trips (duration less than 3 mins)
+# these trips would also have the exact same starting/ending station
+# duration column is in seconds
+# 3 minutes is equal to 3 x 60 = 180 seconds
+cancelled_trips <- trip_v1$id[trip_v1$start_station_id 
+                              == trip_v1$end_station_id
+                              & trip_v1$duration < 180]
+
+# filtering out rows with cancelled trips from the dataset
+trip_v1 <- trip_v1 %>%
+  filter(!(start_station_id == end_station_id 
+           & duration < 180))
+
+# trip IDs saved under cancelled_trips data frame
+# save as a csv file for inclusion in the report
+write.csv(cancelled_trips, "cancelled_trips.csv", row.names = F)
+
+# from the EDA, the p_01 value is 128.0 and the p_99 value is 13311.62
+# for the duration column (in seconds)
+# however, trips shorter than 128 seconds (approx. 2 mins) or longer than 
+# 13311 seconds (approx 3.7 hours) could still be realistic bike rental
+# timeframes
+# therefore, unrealistic trip length can be considered any trip under 
+# 1 minute (60 seconds) in length, or over 6 hours in length (21600 seconds)
+# values outside of this range (below 60 and above 21600) will be treated 
+# as outliers, and the row will be removed from the dataset
+# recording trip IDs for outlier trips
+outlier_trips <- trip_v1$id[trip_v1$duration < 60 | trip_v1$duration > 21600]
+
+# filtering out rows with outliers from the dataset
+trip_v1 <- trip_v1 %>%
+  filter(!(duration < 60 | duration > 21600))
+
+# trip IDs saved under outlier_trips data frame
+# save as a csv file for inclusion in the report
+write.csv(outlier_trips, "outlier_trips.csv", row.names = F)
