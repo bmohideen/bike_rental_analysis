b41f9bc14fac49840ac35559ae6c57eb36f4e155
bmohideen <70986859+bmohideen@users.noreply.github.com>
2024-08-06 09:21:21 -0400

    EDA for the station dataset

--- main.R
+++ main.R
@@ -2,6 +2,8 @@
 # Bilal Mohideen - BTC1855H
 # RStudio Version 2024.04.1+748 (2024.04.1+748)
 
+#### Libraries/Datasets ####
+
 # loading the required packages for the project
 library(funModeling)
 library(Hmisc)
@@ -14,8 +16,34 @@ station <- read.csv("station.csv")
 trip <- read.csv("trip.csv")
 weather <- read.csv("weather.csv")
 
-#### EDA ####
+#### EDA - Overview ####
 
 # performing exploratory data analysis (EDA) for each dataset separately
 # following template outlined here: 
-#https://blog.datascienceheroes.com/exploratory-data-analysis-in-r-intro/
+# https://blog.datascienceheroes.com/exploratory-data-analysis-in-r-intro/
+# data will be transformed during data cleaning/preparation stage
+# this will be an informative analysis; therefore, the original datasets
+# can be used (duplicating to create a copy is not necessary)
+
+#### EDA - Station ####
+# viewing overview and structure of "station" dataframe
+glimpse(station)
+
+# summary of the dataframe (quantity of zeros/NAs/infinite values, data 
+# types, and unique values)
+print(status(station))
+
+# city is the only categorical variable that makes sense to analyze
+# the others are all unique (names) or irrelevant to the project (installation
+# date)
+freq(station)
+freq(station$city)
+
+# frequencies are irrelevant for ID and longitude/latitude
+# dock_count is the most relevant integer variable
+plot_num(station)
+print(profiling_num(station))
+
+# descriptive statistics for the entire dataset
+# provides info on missing and distinct values for each column
+describe(station)
